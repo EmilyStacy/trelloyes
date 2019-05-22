824876aa3bf2f824936fd743667f7f9f4e5dcd6d
EmilyStacy <46908536+EmilyStacy@users.noreply.github.com>
2019-05-21 21:03:25 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Card from './LIST/CARD/Card';
+import List from './LIST/List';
+import renderer from 'react-test-renderer';
+import { prependOnceListener } from 'cluster';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe ('Card', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Card />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+    it('renders the UI as expected', () => {
+        const tree = renderer
+        .create( <div className="Card" id="a"/>).toJSON();
+        expect(tree).toMatchSnapshot(); 
+    });
+}); 
+
+describe ('List', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        const array = List.cards.map();
+        const Card = <List><Card key="a" cardkey="a" title="First Card" content ="lorem ipsum"></Card></List> 
+        ReactDOM.unmountComponentAtNode(div);
+    })
+
+    it('renders the UI as expected', () => {
+        const tree = renderer
+        .create (<div className="List-cards" />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
 });
