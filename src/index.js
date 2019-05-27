c55e2b07c94f946cfdcba50c8da9acc015560a24
EmilyStacy <46908536+EmilyStacy@users.noreply.github.com>
2019-05-27 15:18:50 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+ReactDOM.render(<App store={STORE} />, document.getElementById('root'));
+/*{console.log(STORE)}*/
