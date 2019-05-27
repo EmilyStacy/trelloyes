614736f05786bef7e1d8dd349adcc162b820f341
EmilyStacy <46908536+EmilyStacy@users.noreply.github.com>
2019-05-27 15:37:48 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-ReactDOM.render(<App store={STORE} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 /*{console.log(STORE)}*/
