36c8ff45b33ddcc4a63a2358ed02f2f11c8b6f13
EmilyStacy <46908536+EmilyStacy@users.noreply.github.com>
2019-05-27 11:57:41 -0500

    Add files via upload

---
+++ List.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Card from './CARD/Card';
+import './List.css';
+export default function List(props){
+        return (
+        <section className="List">
+        <div className="List-cards">
+        <h2>{props.header}</h2>
+        {props.cards.map((card) =>
+            <div key= {card.id}> 
+            <Card 
+                  key = {card.id}
+                  cardkey = {card.id}
+                  title = {card.title}
+                  content = {card.content} 
+                  onDeleteItem = {props.onDeleteItem}/>
+            </div>
+
+        )}
+         <button type = 'button' className= "List-add-button" onClick={()=>props.onAddItem(props.id)}>
+              + Add Random Card
+             </button>
+             </div>
+             </section>
+              )
+         }
+         //what is this?
+         List.defaultProps = {
+            onClickAdd: () => {},
+          }
+    // <section className="List">
+    //     <header className="List-header">
+    //         <h2>{props.header}</h2>
+    //     </header>
+    //     <div className="List-cards">  
+    //     { console.log(props) }
+    //         {props.cards.map((card)=>
+    //             <Card 
+    //              key= {card.id}
+    //              cardkey= {card.id}
+    //              title = {card.title}
+    //              content = {card.content} /> 
+    //         )}
+    //         <button type = 'button' className= "List-add-button">
+    //          + Add Random Card
+    //         </button>
+    //     </div>
+    // </section>     
+       
