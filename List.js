b7b25c2d278ad09e987cabe2215f8c635ad0b8a2
EmilyStacy <46908536+EmilyStacy@users.noreply.github.com>
2019-05-27 14:39:23 -0500

    Update List.js

--- List.js
+++ List.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import Card from './CARD/Card';
 import './List.css';
+const ConsoleLog = ({ children }) => {
+    console.log(children);
+    return false;
+  };
 export default function List(props){
         return (
         <section className="List">
         <div className="List-cards">
         <h2>{props.header}</h2>
-        {props.cards.map((card) =>
-            <div key= {card.id}> 
+        {props.cards.map((card) =>{
+            console.log(card.id)
+            return(
+        //<div key= {card.id}> 
             <Card 
                   key = {card.id}
                   cardkey = {card.id}
                   title = {card.title}
-                  content = {card.content} 
-                  onDeleteItem = {props.onDeleteItem}/>
-            </div>
-
+                  content = {card.content}
+                  onDeleteItem = {props.onDeleteItem}/> 
+                  ) 
+            //</div>
+            
+        }            
         )}
          <button type = 'button' className= "List-add-button" onClick={()=>props.onAddItem(props.id)}>
               + Add Random Card
