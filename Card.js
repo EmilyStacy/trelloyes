0b5dbc3004acdc1bfa12854e3161ed347ad38ec4
EmilyStacy <46908536+EmilyStacy@users.noreply.github.com>
2019-05-27 14:39:44 -0500

    Update Card.js

--- Card.js
+++ Card.js
@@ -3,12 +3,13 @@ import './Card.css';
 export default function Card(props){
         return(
         <div className="Card">
-        <button type = 'button' onClick ={()=>props.onDeleteItem(props.id)}>delete</button>
+        <button type = 'button' onClick ={()=>{console.log(props.id,':mycardId')
+        props.onDeleteItem(props.id)}}>delete</button>
         <h3>{props.title}</h3>
         <p>{props.content}</p>
         </div>
+       
         )
-    
 }
 //what is this?
 Card.propTypes = {
